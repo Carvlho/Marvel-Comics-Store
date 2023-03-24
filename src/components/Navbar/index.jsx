1a8882aa7723158658b90950fad87fdae22421d4
Igor Carvalho <igorshadow15@gmail.com>
2023-03-24 12:27:06 -0300

    Revert "Revert "Adicionado uma div para mostrar quandos itens tem no carrinho ao icone de carrinho""
    This reverts commit 5cef0fe6469fa0b5380a0975899e7c275945624d.

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import MarvelLogoSVG from "@/assets/svg/marvel_logo.svg";
 import MarvelLogo from "@/assets/marvel_logo.png";
 import { ReactComponent as IconCart } from "@/assets/svg/IconCart.svg";
 
-import { ContainerNavbar, ContentNavbar, Logo } from "./styles";
+import sumItemsOnCart from "@/utils/sumItemsOnCart";
+
+import { ButtonCart, ContainerNavbar, ContentNavbar, Logo } from "./styles";
 
 export default function Navbar({ openCart }) {
+  const [items, setItems] = useState(0);
+
+  const handleItems = () => {
+    const res = sumItemsOnCart();
+
+    if (res != items) {
+      setItems(res);
+    }
+
+    return res;
+  };
+
   return (
     <ContainerNavbar>
       <ContentNavbar>
@@ -14,11 +28,13 @@ export default function Navbar({ openCart }) {
           <source srcSet={MarvelLogoSVG} type="image/svg+xml" />
           <img src={MarvelLogo} alt="Marvel Logo" />
         </Logo>
-        <IconCart
-          arial-label="Icone do carrinho"
-          fill="#fff"
-          onClick={openCart}
-        />
+        <ButtonCart onClick={openCart}>
+          <IconCart arial-label="Icone do carrinho" fill="#fff" />
+
+          {handleItems() > 0 ? (
+            <div className="numberItems">{items}</div>
+          ) : null}
+        </ButtonCart>
       </ContentNavbar>
     </ContainerNavbar>
   );
